ca66d94a3c57d0403394bca56da324db80527ee7
KwakIron <guozhenzhu0517@gmail.com>
2017-09-24 12:31:40 +0800

    Initial commit

---
+++ app/src/main/java/com/guozhe/android/autocompletetextview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.guozhe.android.autocompletetextview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+    //하나의 단어만 자동
+    private AutoCompleteTextView atv_content;
+    //여러개의 단어 자동
+    private MultiAutoCompleteTextView matv_content;
+
+    private static final String[] data = new String[]{
+            "하늘나라", "개발자", "슈퍼맨", "부들부들", "ㅋㅋㅋㅋ"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        atv_content = (AutoCompleteTextView) findViewById(R.id.atv_content);
+        matv_content = (MultiAutoCompleteTextView) findViewById(R.id.matv_content);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,data);
+        atv_content.setAdapter(adapter);
+
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,data);
+        matv_content.setAdapter(adapter2);
+        //단어하나마다 자동으로 점 찍기 
+        matv_content.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+    }
+}
